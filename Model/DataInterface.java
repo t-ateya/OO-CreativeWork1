360abe532deda18599288d2fd98309edc9e77097
Terence Ateya <ateyaterence@gmail.com>
2021-01-27 16:16:55 -0600

    first commit

---
+++ Model/DataInterface.java
@@ -0,0 +1,7 @@
+package Model;
+
+public interface DataInterface {
+	String loadData();
+	void updateData();
+	
+}
---
+++ Model/DataModel.java
@@ -0,0 +1,17 @@
+package Model;
+
+public class DataModel implements DataInterface{
+
+	@Override
+	public String loadData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateData() {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
