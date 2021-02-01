66d1ec08b8b05aec4fc4a836c558d233c640bd25
Terence Ateya <ateyaterence@gmail.com>
2021-02-01 13:13:57 -0600

    next Commit

--- Model/DataModel.java
+++ Model/DataModel.java
@@ -1,5 +1,8 @@
 package Model;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -11,42 +14,39 @@ import java.nio.file.Files;
 import java.nio.file.Paths;;
 
 public class DataModel implements DataModelInterface {
-
-	
+	private String data = null;
 
 	@Override
 	public String loadData() throws FileNotFoundException, IOException {
-		if (!(new File ("file.txt").isFile())){
+		if (!(new File("file.txt").isFile())) {
 			System.out.println("File does not exits-I am creating the file");
 			Files.createFile(Paths.get("file.txt"));
 		}
 
-		String data = null;
-		//Let's use a try-with-resource block
-		try(BufferedReader reader = new BufferedReader(new FileReader("file.txt"))){
+		//String data = null;
+		// Let's use a try-with-resource block
+		try (BufferedReader reader = new BufferedReader(new FileReader("file.txt"))) {
 			StringBuilder string = new StringBuilder();
 			String line = reader.readLine();
 			string.append("<html>\n");
-			while (line !=null){
+			while (line != null) {
 				string.append(line);
 				string.append(" <br />\n");
-				line= reader.readLine();
+				line = reader.readLine();
 			}
 			string.append("\n</html>");
 			data = string.toString();
-		}catch (Exception ex){
+		} catch (Exception ex) {
 			data = ex.getMessage();
 		}
 		return data;
 
-	
 	}
-
 
 	@Override
 	public boolean updateData(String data) {
-		
-		//Save data to the file
+
+		// Save data to the file
 		try (BufferedWriter writer = new BufferedWriter(new FileWriter("file.txt"))) {
 			writer.write(data);
 			return true;
@@ -54,7 +54,19 @@ public class DataModel implements DataModelInterface {
 			e.printStackTrace();
 			return false;
 		}
-		
+
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	@Override
+	public void render(Graphics2D g2) {
+		g2.setColor(Color.yellow);
+		g2.setFont(new Font("Courier", Font.BOLD, 14));
+		g2.drawString(data, 50, 100);
+
 	}
 
 
--- View/DataCanvas.java
+++ View/DataCanvas.java
@@ -2,8 +2,13 @@ package View;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
+
+import Model.DataModel;
+import Model.DataModelInterface;
 
 public class DataCanvas extends JPanel{
 	private DataPanel panel;
@@ -11,8 +16,15 @@ public class DataCanvas extends JPanel{
 	public DataCanvas(DataPanel panel ){
 		this.panel = panel;
 		setPreferredSize(new Dimension(500, 500));
-
 		setBackground(Color.BLACK);
 	}
 	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		Graphics2D g2 = (Graphics2D)g;
+		DataModelInterface d = new DataModel();
+		d.render(g2);
+	}
 }
